b89b0eac81649762d299dc736088af97a38675b1
AdamWhitehurst <Adam.Whitehurst@live.com>
2019-02-19 10:00:46 -0800

    Fix typings

--- src/sprite/sprite_window.js
+++ src/sprite/sprite_window.js
@@ -1,8 +1,8 @@
-import * as P from "pixi.js";
 import { ipcRenderer } from "electron";
-P.settings.SCALE_MODE = P.SCALE_MODES.NEAREST;
+import "pixi.js";
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 let dim = 64;
-let pixi = new P.Application(dim, dim, { transparent: true });
+let pixi = new PIXI.Application(dim, dim, { transparent: true });
 let spriteName, currSprite;
 let animations;
 let defaultAction = {
@@ -14,7 +14,7 @@ let defaultAction = {
   }
 };
 function loadSpritesheet() {
-  P.loader
+  PIXI.loader
     .add("green_slime", "../../assets/sheets/green_slime.json")
     .load(() => {
       spriteName = ipcRenderer.sendSync("window-ready");
@@ -22,13 +22,12 @@ function loadSpritesheet() {
     });
 }
 function onSpritesheetLoaded() {
-  animations = P.loader.resources[spriteName].spritesheet.animations;
+  animations = PIXI.loader.resources[spriteName].spritesheet.animations;
   playAnim("idle", defaultAction);
 }
 function loadSprite(anim) {
   console.log(anim);
-
-  let as = new P.extras.AnimatedSprite(animations[anim]);
+  let as = new PIXI.extras.AnimatedSprite(animations[anim]);
   as.interactive = true;
   as.animationSpeed = 0.15;
   as.x = pixi.screen.width / 2;
